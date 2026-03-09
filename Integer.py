7dce453a6adfb55d8723ed711e768602f99ce1df
vvreddy51 <156112543+vvreddy51@users.noreply.github.com>
2026-03-09 17:42:53 +0530

    Add basic arithmetic operations in Integer.py

---
+++ Integer.py
@@ -0,0 +1,29 @@
+num1=20
+num2=40
+
+## devision
+##result1= num2//num1
+print("Devision:", num2//num1)
+
+#absolute value
+num3=-55
+
+result2=abs(num3)
+print("Absolute value:", result2)
+
+## addition
+result3=num1+num2+num3
+print("Addition:", result3) 
+
+## round value
+num4=4.3246744
+result4=round(num4,2)
+print("Round value:", result4)
+
+##floating vlaues
+num5=3.12
+num6=2.37
+
+result5=num5+num6
+print("Float value:", result5)
+
